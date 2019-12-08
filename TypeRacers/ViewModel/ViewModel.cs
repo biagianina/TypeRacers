0087d6ea2d79cdc5f5fbf6c9873a72e35f6978d8
thisisalin <alintroncea@gmail.com>
2019-12-08 21:54:51 +0200

    changes

--- TypeRacers/ViewModel/ViewModel.cs
+++ TypeRacers/ViewModel/ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Documents;
@@ -17,9 +18,8 @@ namespace TypeRacers.ViewModel
         readonly Model.Model model;
         int correctChars;
         int incorrectChars;
-        string progress;
+        string progress ="";
         private bool allTextTyped;
-        int currentWord;
 
         public ViewModel()
         {
@@ -55,16 +55,11 @@ namespace TypeRacers.ViewModel
                 };
         }
 
-        public int CurrentWordLength
-        {
-            get => TextToType.Split()[currentWord++].Length;
-        }
-
         public string Progress
-        {            
+        {
             get
             {
-               return progress = (spaceIndex * 100 / TextToType.Length).ToString() + "%";
+                return progress = (spaceIndex * 100 / TextToType.Length).ToString() + "%";
             }
         }
         public string CurrentInputText
@@ -85,11 +80,10 @@ namespace TypeRacers.ViewModel
                 if (isValid && value.EndsWith(" ") || text.Length + spaceIndex == TextToType.Length)
                 {
                     spaceIndex += text.Length;
-                    TriggerPropertyChanged(nameof(CurrentWordLength));
                     TriggerPropertyChanged(nameof(Progress));
                     dataValidation = new InputCharacterValidation(TextToType.Substring(spaceIndex));
                     text = "";
-                    model.GetClient.SendMessageToServer(progress);
+                    SendProgress();
                 }
 
                 //determine number o characters taht are valid/invalid to form substrings
@@ -104,6 +98,11 @@ namespace TypeRacers.ViewModel
 
                 TriggerPropertyChanged(nameof(CurrentInputText));
             }
+        }
+
+        private void SendProgress()
+        {
+            model.GetClient.SendMessageToServer(progress);
         }
 
         public bool AllTextTyped
