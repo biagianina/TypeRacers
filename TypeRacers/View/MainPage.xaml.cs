6f941b5dc851a2a39064e435ff4b6f3a6fc82a9d
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2020-01-14 22:48:26 +0200

    Added ViewModel for MainWindow

--- TypeRacers/View/MainPage.xaml.cs
+++ TypeRacers/View/MainPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TypeRacers.ViewModel;
 
 namespace TypeRacers.View
 {
@@ -25,16 +15,13 @@ namespace TypeRacers.View
             InitializeComponent();
         }
 
-        private void Contest_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService nav = NavigationService.GetNavigationService(this);
-            nav.Navigate(new Uri("View/VersusPage.xaml", UriKind.RelativeOrAbsolute));
-        }
+        private MainViewModel ViewModel
+            => (MainViewModel)Resources["MainVM"];
 
-        private void Practice_Click(object sender, RoutedEventArgs e)
+        private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            NavigationService nav = NavigationService.GetNavigationService(this);
-            nav.Navigate(new Uri("View/PracticePage.xaml", UriKind.RelativeOrAbsolute));
+            ViewModel.ContestNavigation = NavigationService.GetNavigationService(this);
+            ViewModel.PracticeNavigation = NavigationService.GetNavigationService(this);
         }
     }
 }
---
+++ TypeRacers/ViewModel/Commander.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TypeRacers.ViewModel
+{
+    public class CommandHandler : ICommand
+    {
+        private readonly Action toExecute;
+        private readonly Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public CommandHandler(Action action, Func<bool> canExecute)
+        {
+            toExecute = action;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute.Invoke();
+        }
+
+        public void Execute(object parameter)
+        {
+            toExecute();
+        }
+    }
+}
