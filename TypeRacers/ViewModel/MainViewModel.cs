e36e1408511ad945daa2867d22df10e8b76829f7
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2020-01-14 22:50:43 +0200

    trying to solve conflicts

---
+++ TypeRacers/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Navigation;
+
+namespace TypeRacers.ViewModel
+{
+    internal class MainViewModel
+    {
+        public MainViewModel()
+        {
+            ContestCommand = new CommandHandler(() => NavigateContest(), () => true);
+            PracticeCommand = new CommandHandler(() => NavigatePractice(), () => true);
+        }
+
+        public CommandHandler ContestCommand { get; }
+
+        public NavigationService ContestNavigation { get; set; }
+
+        public CommandHandler PracticeCommand { get; }
+
+        public NavigationService PracticeNavigation { get; set; }
+
+        private void NavigateContest()
+        {
+            ContestNavigation.Navigate(new Uri("View/VersusPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void NavigatePractice()
+        {
+            PracticeNavigation.Navigate(new Uri("View/PracticePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
