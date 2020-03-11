8a63784221a216ffca03e1e07d2a8db720d8d912
biagianina <53540842+biagianina@users.noreply.github.com>
2020-03-11 18:35:58 +0200

    added char validator tests

---
+++ TypeRacers.Tests/InputValidationTests.cs
@@ -0,0 +1,81 @@
+﻿using Xunit;
+using TypeRacers;
+using System.Windows;
+using System.Threading;
+
+namespace TypeRacersFacts
+{
+    public class InputValidationTests
+    {
+        [Fact]
+        public void FirstCharValidator()
+        {
+            Thread validate = new Thread(()=> { var validator = new InputCharacterValidation("This is the text.");
+                validator.ValidateInput("T");
+                Assert.True(validator.IsValid);
+                Assert.False(validator.TypingAlert);
+                Assert.Equal(4, validator.CurrentWordLength);
+                Assert.Equal(0, validator.CurrentWordIndex);
+                Assert.False(validator.AllTextTyped);
+                Assert.False(validator.OpenFinishPopup);
+                Assert.False(validator.Clear);
+                Assert.Equal(1, validator.NumberOfCharactersTyped);
+            });
+            validate.Start();
+        }
+
+        [Fact]
+        public void FirstWordValidator()
+        {
+            Thread validate = new Thread(() => {
+                var validator = new InputCharacterValidation("This is the text.");
+                validator.ValidateInput("This ");
+                Assert.True(validator.IsValid);
+                Assert.False(validator.TypingAlert);
+                Assert.Equal(2, validator.CurrentWordLength);
+                Assert.Equal(1, validator.CurrentWordIndex);
+                Assert.False(validator.AllTextTyped);
+                Assert.False(validator.OpenFinishPopup);
+                Assert.True(validator.Clear);
+                Assert.Equal(5, validator.NumberOfCharactersTyped);
+            });
+            validate.Start();
+        }
+
+        [Fact]
+        public void OneIncorectCharValidator()
+        {
+            Thread validate = new Thread(() => {
+                var validator = new InputCharacterValidation("This is the text.");
+                validator.ValidateInput("Thiz");
+                Assert.False(validator.IsValid);
+                Assert.True(validator.TypingAlert);
+                Assert.Equal(4, validator.CurrentWordLength);
+                Assert.Equal(0, validator.CurrentWordIndex);
+                Assert.False(validator.AllTextTyped);
+                Assert.False(validator.OpenFinishPopup);
+                Assert.False(validator.Clear);
+                Assert.Equal(4, validator.NumberOfCharactersTyped);
+            });
+            validate.Start();
+        }
+
+        [Fact]
+        public void WholeTextValidator()
+        {
+            Thread validate = new Thread(() => {
+                var validator = new InputCharacterValidation("This is the text.");
+                validator.ValidateInput("This is the text.");
+                Assert.True(validator.IsValid);
+                Assert.False(validator.TypingAlert);
+                Assert.Equal(5, validator.CurrentWordLength);
+                Assert.Equal(3, validator.CurrentWordIndex);
+                Assert.True(validator.AllTextTyped);
+                Assert.True(validator.OpenFinishPopup);
+                Assert.False(validator.Clear);
+                Assert.Equal(17, validator.NumberOfCharactersTyped);
+            });
+            validate.Start();
+        }
+    }
+}
